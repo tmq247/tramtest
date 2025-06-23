6aeaf37259fded7e23eabce6b872ab5d5475de73
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-06-23 18:17:14 +0700

    Update call.py

--- SANKIXD/core/call.py
+++ SANKIXD/core/call.py
@@ -7,8 +7,6 @@ from typing import Union
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
 from pytgcalls import PyTgCalls
-from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
 
 import config
 from SANKIXD import LOGGER, YouTube, app
