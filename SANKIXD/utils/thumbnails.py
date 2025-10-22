ba93ae0d6744f9f516049cfa8f787fd9e2db4f43
tmq247 <60788746+tmq247@users.noreply.github.com>
2025-10-22 14:29:39 +0700

    Update thumbnails.py

--- SANKIXD/utils/thumbnails.py
+++ SANKIXD/utils/thumbnails.py
@@ -4,7 +4,7 @@ import aiofiles
 import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
 from unidecode import unidecode
-from youtubesearchpython.__future__ import VideosSearch
+from py_yt import VideosSearch
 from SANKIXD import app
 from config import YOUTUBE_IMG_URL
 
